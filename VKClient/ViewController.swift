72c8ad9551ecac8eb1aab910bd2cd5cd259552e6
Vadim Akhmarov <riningan@gmail.com>
2022-01-09 18:20:07 +0500

    Initial Commit

---
+++ VKClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKClient
+//
+//  Created by Вадим Ахмаров on 09.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
